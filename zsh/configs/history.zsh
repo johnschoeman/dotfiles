e33413f8c2a0ab123c4ec4153736487f29de3602
John Schoeman <johnschoeman1617@gmail.com>
2021-04-05 17:33:05 -0700

    Increase zsh history size

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,6 +1,7 @@
 setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
-HISTSIZE=4096
-SAVEHIST=4096
+HISTFILESIZE=1000000000
+HISTSIZE=1000000
+SAVEHIST=1000000
 
 export ERL_AFLAGS="-kernel shell_history enabled"
