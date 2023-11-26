34c964136c5ff7b7ad1850d64be53ff3e1b9f64b
John Schoeman <johnschoeman1617@gmail.com>
2023-11-26 16:40:49 -0600

    Add language server support for neovim
    Why:
    
    We'd like to use language servers.
    
    This commit:
    
    Adds general lsp support configuration and typescript-tools for our
    typescript lsp.
    
    https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/
    https://github.com/pmizio/typescript-tools.nvim

--- vim/init.lua
+++ vim/init.lua
@@ -96,7 +96,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end
 })
 
--- TODO: Resize panes
+-- TODO: Prettier and lint:fix
 
 -- ========================================================================== --
 -- ==                               PLUGINS                                == --
@@ -116,11 +116,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
-  { 'folke/tokyonight.nvim', lazy = false, priority = 1000 },
+  -- Theme
   { 'rose-pine/neovim', name = 'rose-pine', lazy = false },
   { 'nvim-tree/nvim-web-devicons' },
   { 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true } },
+
+  -- Navigation
   { 'christoomey/vim-tmux-navigator' },
+
+  -- Code manipulation
   { 'tpope/vim-surround' },
   { 'tpope/vim-repeat' },
   { 'lukas-reineke/indent-blankline.nvim', main = "ibl", opts = {} },
@@ -128,12 +132,39 @@ plugins = {
   { 'nvim-treesitter/nvim-treesitter-textobjects' },
   { 'wellle/targets.vim' },
   { 'numToStr/Comment.nvim', opts = {}, lazy = false },
-  { 'nvim-lua/plenary.nvim' },
+
+  -- Fuzzy finding
   { 'nvim-telescope/telescope.nvim' },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+
+  -- Git
   { 'tpope/vim-fugitive' },
+
+  -- Utilities
   { 'moll/vim-bbye' },
+  { 'nvim-lua/plenary.nvim' },
+
+  -- AI
   { 'github/copilot.vim' },
+
+  -- LSP support
+  { 'neovim/nvim-lspconfig' },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+
+  -- Auto complete
+  { 'hrsh7th/nvim-compe' }, 
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-cmdline' },
+  { 'hrsh7th/nvim-cmp' },
+
+  -- Snippets
+  { 'L3MON4D3/LuaSnip' },
 }
 
 opts = {}
@@ -153,6 +184,7 @@ vim.cmd [[ colorscheme rose-pine ]]
 
 -- TODO: toggle between light and dark theme
 
+
 ---
 -- lualine.nvim (statusline)
 ---
@@ -165,11 +197,13 @@ require('lualine').setup({
   }
 })
 
+
 ---
 -- netrw (file explorer)
 ---
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
+
 
 ---
 -- indent-blankline
@@ -179,6 +213,7 @@ require('ibl').setup({
     char = '|',
   },
 })
+
 
 ---
 -- text-objects (treesitter)
@@ -218,10 +253,12 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
+
 ---
 -- Comments
 ---
 require('Comment').setup({})
+
 
 ---
 -- Fuzzy Finder (telescope)
@@ -235,12 +272,213 @@ vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>'
 
 require('telescope').load_extension('fzf')
 
+
 ---
 -- vim-bbye
 ---
 vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
 
--- ========================================================================== --
--- ==                           LANGUAGE SERVER                            == --
--- ========================================================================== --
 
+---
+-- Luasnip (snippet engine)
+---
+-- See :help luasnip-loaders
+require('luasnip.loaders.from_vscode').lazy_load()
+
+
+---
+-- nvim-cmp (autocomplete)
+---
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+-- See :help cmp-config
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    fields = {'menu', 'abbr', 'kind'},
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        luasnip = '⋗',
+        buffer = 'Ω',
+        path = '🖫',
+      }
+
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
+  -- See :help cmp-mapping
+  mapping = {
+    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    ['<C-f>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+
+    ['<C-b>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      local col = vim.fn.col('.') - 1
+
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+  },
+})
+
+
+---
+-- Diagnostic customization
+---
+local sign = function(opts)
+  -- See :help sign_define()
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = '✘'})
+sign({name = 'DiagnosticSignWarn', text = '▲'})
+sign({name = 'DiagnosticSignHint', text = '⚑'})
+sign({name = 'DiagnosticSignInfo', text = '»'})
+
+-- See :help vim.diagnostic.config()
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
+})
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
+
+---
+-- LSP Keybindings
+---
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = group,
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- You can search each function in the help page.
+    -- For example :help vim.lsp.buf.hover()
+
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    bufmap('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<cr>')
+    bufmap({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  end
+})
+
+
+---
+-- LSP servers
+---
+
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Prevent multiple instance of lsp servers
+-- if file is sourced again
+if vim.g.lsp_setup_ready == nil then
+  vim.g.lsp_setup_ready = true
+
+  -- See :help lspconfig-setup
+  lspconfig.html.setup({capabilities = lsp_capabilities,})
+  lspconfig.cssls.setup({capabilities = lsp_capabilities,})
+  lspconfig.eslint.setup({capabilities = lsp_capabilities,})
+end
+
+-- typescript
+require("typescript-tools").setup({})
+
+-- LSP Floating Windows
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
